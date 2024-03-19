3332dfa5d7cdda9cd5c228d454a3689617882112
Сергей Попов <sergey1404sergey@gmail.com>
2024-03-19 20:29:48 +0300

    Удаление комментов.

--- backend/recipes/permissions.py
+++ backend/recipes/permissions.py
@@ -7,10 +7,3 @@ class IsOwnerOrReadOnly(BasePermission):
         return request.method in SAFE_METHODS or (
             obj.author == request.user or request.user.is_staff
         )
-
-
-# class IsOwnerOrAdmin(BasePermission):
-#     def has_object_permission(self, request, view, obj):
-#         if request.method in SAFE_METHODS:
-#             return True
-#         return obj.author == request.user or request.user.is_staff
