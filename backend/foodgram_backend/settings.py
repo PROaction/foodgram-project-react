ac4b5a44afcd4dcb5d85143d0e316cb6a60ec599
Сергей Попов <sergey1404sergey@gmail.com>
2024-03-19 17:28:30 +0300

    isort.

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -1,9 +1,10 @@
-import os
 from datetime import timedelta
+import os
 from pathlib import Path
 
 from django.core.management.utils import get_random_secret_key
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
