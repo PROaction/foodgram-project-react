948f5142acc973b54071c939e09b3409df2f7f9c
Сергей Попов <sergey1404sergey@gmail.com>
2024-03-20 15:38:49 +0300

    Фикс flake8.

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -42,13 +42,13 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny',],
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny'],
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE':
         6,
     'DEFAULT_AUTHENTICATION_CLASSES':
-        ['rest_framework.authentication.TokenAuthentication',],
+        ['rest_framework.authentication.TokenAuthentication'],
 }
 
 ROOT_URLCONF = 'foodgram_backend.urls'
