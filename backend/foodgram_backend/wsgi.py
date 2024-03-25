165bb43b3559fcfff47d11caa9b869bcaf7bde42
Сергей Попов <sergey1404sergey@gmail.com>
2024-03-25 12:40:41 +0300

    Исправление подписок.

--- backend/foodgram_backend/wsgi.py
+++ backend/foodgram_backend/wsgi.py
@@ -3,7 +3,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-# ew
+# e
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodgram_backend.settings')
 
 application = get_wsgi_application()
